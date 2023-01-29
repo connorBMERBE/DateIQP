bb38bc6cdf43b049081e20982bed79f1debd218a
ebhoffberg <87536786+ebhoffberg@users.noreply.github.com>
2023-01-29 13:52:57 +0200

    subtractions and more test images
    date_detector can subtract two images, but does not yet have detection abilities

---
+++ date_detector.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+img=cv2.imread("DateIQP\\tutorials\\assets\\blank.jpg")
+img1=cv2.imread("DateIQP\\tutorials\\assets\\date.jpg")
+sub=cv2.subtract(img,img1)
+gray=cv2.cvtColor(sub,cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray,(3,3),0)
+contours, _= cv2.findContours(blur,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+c=max(contours,key=cv2.contourArea)
+im = cv2.resize(sub, (800,800))
+# Set up the detector with default parameters.
+detector = cv2.SimpleBlobDetector_create()
+
+# Detect blobs.
+keypoints = detector.detect(im)
+ 
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+ 
+# Show keypoints
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
