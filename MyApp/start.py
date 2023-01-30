70409ae17ce44c14b534e4d0febe8c4a4424b09e
Matt Adam <67502293+GitGuitar@users.noreply.github.com>
2023-01-30 15:31:52 +0200

    Create start.py

---
+++ MyApp/start.py
@@ -0,0 +1,20 @@
+class IsolateDate: 
+    def __init__(self, image):
+        self.image = image
+        
+        self.t1 = 85
+        self.t2 = 255
+        
+    def contour(): 
+        pass
+    
+    def image_subtraction(): 
+        pass
+
+
+# --- 
+
+import cv2 
+import numpy as np
+
+img = cv2.imread
