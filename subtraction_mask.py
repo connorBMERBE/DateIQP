7932fa58c0c8fc0e308f81f5cc98ed1c859eb620
ebhoffberg <87536786+ebhoffberg@users.noreply.github.com>
2023-01-30 10:49:45 +0200

    recommit of subtraction_mask
    weird GitHub issues needed a recommit

--- subtraction_mask.py
+++ subtraction_mask.py
@@ -1,16 +1,22 @@
+#imports
 import numpy as np
 import cv2
 
-img=cv2.imread("DateIQP\\tutorials\\assets\\blank.jpg") #blank image of tray (control variable)
-img1=cv2.imread("DateIQP\\tutorials\\assets\\date.jpg") #test image of date on tray
-sub=cv2.subtract(img,img1) #subtraction of empty tray from date on tray to isolate date
-im = cv2.resize(sub, (800,800)) #resizes image to a readable scale
-im2 = cv2.resize(img1, (800, 800)) #resizes image to a readable scale
-im = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+# Load in the images from the file:
+control=cv2.imread("DateIQP\\tutorials\\assets\\blank.jpg") #blank image of tray (control variable)
+test=cv2.imread("DateIQP\\tutorials\\assets\\date.jpg") #test image of date on tray
+
+#Creating the mask:
+sub=cv2.subtract(control,test) #subtraction of empty tray from date on tray to isolate date
+sub = cv2.resize(sub, (800,800)) #resizes image to a readable scale
+test = cv2.resize(test, (800, 800)) #resizes image to a readable scale
+sub = cv2.cvtColor(sub,cv2.COLOR_BGR2GRAY) #makes subtraction grayscale to create a mask
 black = np.array([15]) #lower bound
-gray = np.array([255]) #upper bound (there is no upper bound)
-mask = cv2.inRange(im, black, gray) # true or false (white or black) if a pixel is within range
-result = cv2.bitwise_and(im2, im2, mask=mask) # only shows pixels within the mask range in test image for result
-# Show keypoints
-cv2.imshow("Subtraction", result) #shows image
+white = np.array([255]) #upper bound (there is no upper bound)
+mask = cv2.inRange(sub, black, white) # true or false (white or black) if a pixel is within range
+result = cv2.bitwise_and(test, test, mask=mask) # only shows pixels within the mask range in test image for result
+
+#Displaying the image:
+cv2.imshow("Subtraction Mask", result) #shows image
 cv2.waitKey(0)
+cv2.destroyAllWindows()
