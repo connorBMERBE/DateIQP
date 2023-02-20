032cb298bb98da356c5bac72d1c356d1bba1861c
ebhoffberg <87536786+ebhoffberg@users.noreply.github.com>
2023-02-20 18:18:32 +0200

    Fixing SQL stuff to be current

--- SQLStuff/SQLStuff.py
+++ SQLStuff/SQLStuff.py
@@ -4,7 +4,7 @@ import numpy as np
 # Connect to server
 mydb = sql.connect(
     host = "localhost",
-    user = "Master",
+    user = "root",
     password = "D@t3MACHINE",
     database = "datesdb"
 )
@@ -19,10 +19,22 @@ cursor = mydb.cursor()
 # is used to compile a SQL statement
 # below statement is used to create
 # the 'geeksforgeeks' database
-cursor.execute("insert into Dates values('proxy\\address','2023-02-18','2023-02-19',000000,12.6,1,0,0,0,0,1,0,0);") 
-
+cursor.execute("insert into trainingdata values(\"file\path00\", 23.12345678, 2.00000001, 70, 0);")
+cursor.execute("insert into trainingdata values(\"file\\path01\", 1, 2, 3, 0);") 
+cursor.execute("insert into trainingdata values(\"file\\path02\", 1, 2, 3, 1);") 
+cursor.execute("insert into trainingdata values(\"file\\path03\", 1, 2, 3, 1);") 
+cursor.execute("insert into trainingdata values(\"file\\path04\", 1, 2, 3, 2);") 
+cursor.execute("insert into trainingdata values(\"file\\path05\", 1, 2, 3, 3);") 
+cursor.execute("insert into trainingdata values(\"file\\path06\", 170.425425, 78.42356436, 54.4254252, 4);") 
+cursor.execute("insert into trainingdata values(\"file\\path07\", 180, 255, 255, 4);") 
+cursor.execute("insert into trainingdata values(\"file\\path08\", 0, 0, 0, 3);") 
+cursor.execute("insert into trainingdata values(\"file\\path09\", 1, 1, 1, 2);") 
+cursor.execute("insert into trainingdata values(\"file\\path10\", 1, 1, 3.45252, 0);") 
+cursor.execute("insert into trainingdata values(\"file\\path11\", 10.45754, 20.24436, 30, 1);") 
+cursor.execute("insert into trainingdata values(\"file\\path12\", 100, 200, 45.557864, 0);") 
 # Show database
-cursor.execute("select * from Dates")
- 
-# for x in cursor:
-#   print(x)
+print(cursor)
+mydb.commit()
+cursor.execute("select * from trainingdata;")
+for x in cursor:
+  print(x)
