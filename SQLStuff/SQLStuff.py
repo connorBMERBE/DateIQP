d557e50eed67b17475351fc49928384e7520d528
DateMachine <SAMOP.DateMachine@gmail.com>
2023-02-20 16:16:34 +0200

    More changes on SQL manual and testing code

--- SQLStuff/SQLStuff.py
+++ SQLStuff/SQLStuff.py
@@ -19,12 +19,22 @@ cursor = mydb.cursor()
 # is used to compile a SQL statement
 # below statement is used to create
 # the 'geeksforgeeks' database
-cursor.execute("insert into testing values(9);") 
-cursor.execute("insert into testing values(6);")
+cursor.execute("insert into trainingdata values(\"file\path00\", 23.12345678, 2.00000001, 70, 0);")
+cursor.execute("insert into trainingdata values(\"file\\path01\", 1, 2, 3, 0);") 
+cursor.execute("insert into trainingdata values(\"file\\path02\", 1, 2, 3, 1);") 
+cursor.execute("insert into trainingdata values(\"file\\path03\", 1, 2, 3, 1);") 
+cursor.execute("insert into trainingdata values(\"file\\path04\", 1, 2, 3, 2);") 
+cursor.execute("insert into trainingdata values(\"file\\path05\", 1, 2, 3, 3);") 
+cursor.execute("insert into trainingdata values(\"file\\path06\", 170.425425, 78.42356436, 54.4254252, 4);") 
+cursor.execute("insert into trainingdata values(\"file\\path07\", 180, 255, 255, 4);") 
+cursor.execute("insert into trainingdata values(\"file\\path08\", 0, 0, 0, 3);") 
+cursor.execute("insert into trainingdata values(\"file\\path09\", 1, 1, 1, 2);") 
+cursor.execute("insert into trainingdata values(\"file\\path10\", 1, 1, 3.45252, 0);") 
+cursor.execute("insert into trainingdata values(\"file\\path11\", 10.45754, 20.24436, 30, 1);") 
+cursor.execute("insert into trainingdata values(\"file\\path12\", 100, 200, 45.557864, 0);") 
 # Show database
 print(cursor)
 mydb.commit()
-cursor.execute("select * from testing;")
+cursor.execute("select * from trainingdata;")
 for x in cursor:
   print(x)
-cursor.execute("drop table testing;")
