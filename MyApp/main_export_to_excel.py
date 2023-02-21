0ef28ad8a26f9d0b7d5aaa46362e3e6f6cfaf613
Matt Adam <matthew.adam20@gmail.com>
2023-02-21 15:30:50 +0200

    progress on export module

--- MyApp/main_export_to_excel.py
+++ MyApp/main_export_to_excel.py
@@ -6,19 +6,39 @@ def dbViewFiltered():
     cursor.execute(f'')    
 
 
-cursor.execite("""SELECT 
-    *
-FROM
-    dates
-WHERE
-    class = value_of_interest
-""")
+# harvestDaySTRING = "2022-02-20"
+# barcode = "123456789"
 
+print("You are running a script that will download a filtered section of the MySQL database running on this computer.")
 
-{'imageAddress':-1, 
+import user_input.py
+
+harvestDaySTRING, barCode = user_input.get()
+
+cursor.execite(f"SELECT * FROM dates WHERE harvestDay = {harvestDaySTRING} AND barCode = {barCode}")
+
+location = {'imageAddress':-1, 
             'harvestDay':harvestDay.strftime('%Y-%m-%d'), 
             'measureDay':measureDay.strftime('%Y-%m-%d'), 
             'barCode':barCode, 
             'weight':weight }
 
 
+
+
+# imageAddress VARCHAR(255) NOT NULL, #image location
+# harvestDay DATE NOT NULL, #day of tree harvest, format YYYY-MM-DD
+# measureDay DATE NOT NULL, #day of date classification, format YYYY-MM-DD
+# barCode INT(6) UNSIGNED NOT NULL, #tree identifier
+# weight DOUBLE(3,1) NOT NULL,
+
+# readyOrJuicy BOOLEAN,
+# moist BOOLEAN,
+# yellow BOOLEAN,
+# halfFirm BOOLEAN,
+# firm BOOLEAN, #(AKA smooth)
+# blistered BOOLEAN,
+# skippedAStage BOOLEAN,
+# dry BOOLEAN,
+
+# CONSTRAINT Dates_PK PRIMARY KEY (imageAddress)    		
