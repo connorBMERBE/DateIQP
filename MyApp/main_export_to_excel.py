1130d3fda24208ce56154a0283e33cd70cb26711
Matt Adam <matthew.adam20@gmail.com>
2023-02-21 14:50:14 +0200

    Update main_export_to_excel.py

---
+++ MyApp/main_export_to_excel.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import sql.connector as sql 
+
+def dbViewFiltered(): 
+
+    cursor.execute(f'')    
+
+
+cursor.execite("""SELECT 
+    *
+FROM
+    dates
+WHERE
+    class = value_of_interest
+""")
+
+
+{'imageAddress':-1, 
+            'harvestDay':harvestDay.strftime('%Y-%m-%d'), 
+            'measureDay':measureDay.strftime('%Y-%m-%d'), 
+            'barCode':barCode, 
+            'weight':weight }
+
+
