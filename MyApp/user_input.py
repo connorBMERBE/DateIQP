65b2371d9350578b84cd2e47b23811f9337b0ea0
Matt Adam <matthew.adam20@gmail.com>
2023-02-21 15:31:00 +0200

    Update user_input.py

--- MyApp/user_input.py
+++ MyApp/user_input.py
@@ -1,8 +1,6 @@
 import datetime 
 
 def get():
-    input('Software startup complete, START the Machine now. Once it is fully on and conveyors are running, Press ENTER key to continue')
-
     while True: 
         entry = input('For this batch, enter the Day Harvested in (YYYY-MM-DD) format: ')
         year, month, day = map(int, entry.split('-'))
